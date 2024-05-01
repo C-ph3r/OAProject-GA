29424c4c0054a0bec2ccd13f8d6f8f138897758a
C-ph3r <margaridaimsardinha@gmail.com>
2024-05-01 19:05:25 +0100

    Update utils.py

--- OA/utils/utils.py
+++ OA/utils/utils.py
@@ -9,10 +9,20 @@ def get_elite_max(population, pop_fit):
 
     return population[np.argmax(pop_fit)], max(pop_fit)
 
-def get_n_elites_max(n:int):
-
+def get_n_elites_max(n: int):
+    
     def get_elite(population, pop_fit):
-        # best n elites
-        return [#list of elites], [#list of their fits]
+        elites = []
+        fits = []
+
+        for i in range(n):
+            curr_elite, curr_fit = get_elite_max(population, pop_fit)
+            elites.append(curr_elite)
+            fits.append(curr_fit)
+            elite_index = np.argmax(pop_fit)
+            population.pop(elite_index)
+            pop_fit.pop(elite_index)
+            
+        return elites, fits
 
     return get_elite
