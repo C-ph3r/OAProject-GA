7e62847ff9261d833018d16fce35823357459db9
C-ph3r <margaridaimsardinha@gmail.com>
2024-05-05 17:06:13 +0100

    generated random individual

--- OA/base/individual.py
+++ OA/base/individual.py
@@ -7,7 +7,7 @@ areas the player passes through in any given session.
 Each index in the vector corresponds to the codename of the area the player visits
 in order. If an area is after the final 'D', it is considered to not be visited.
 
-Example solution: ['D', 'FC', 'G', 'QS', 'QG', 'CS', 'KS', 'RG', DV', 'SN', 'D']
+Example solution: ['D', 'FC', 'G', 'QS', 'DV', 'QG', 'CS', 'SN', 'RG', 'D', 'KS']
 
 Considering the following:
 Dirtmouth (D) - Start and end
@@ -33,38 +33,10 @@ Garden’s (QG).
 • The Resting Grounds (RG) can only be reached in the last half of the session.
 '''
 
-def generate_solution_ks(size):
+areas = ['D', 'FC', 'G', 'QS', 'QG', 'CS', 'KS', 'RG', 'DV', 'SN']
 
-    def generate_sol():
-        return [random.randint(0, 1) for _ in range(size)]
+def generate_random_solution(areas):
+    return ['D'] + random.choices(areas, len(areas), replace=False)
 
-    return generate_sol
-
-
-def get_fitness_ks_max(values, volumes, capacity):
-
-    def get_fitness(sol):
-
-        # getting the total value of the items in the solution
-        fitness = sum([values[i] * sol[i] for i in range(len(sol))])
-
-        # getting the total volume these items occupy
-        total_vol = sum([volumes[i] * sol[i] for i in range(len(sol))])
-
-        return fitness if total_vol <= capacity else 0
-
-    return get_fitness
-
-
-def get_fitness_ks_min(values, volumes, capacity): # TODO: finish/change this
+def generate_possible_solution(areas):
     
-    def get_fitness(sol):
-        # getting the total value of the items in the solution
-        fitness = sum([values[i] * sol[i] for i in range(len(sol))])
-
-        # getting the total volume these items occupy
-        total_vol = sum([volumes[i] * sol[i] for i in range(len(sol))])
-
-        return fitness # we will talk about this soon ;)
-
-    return get_fitness
