add44c1fd5682b8519711694172b35b7b2b3b2f3
C-ph3r <margaridaimsardinha@gmail.com>
2024-05-13 14:11:44 +0100

    tested inversion mutation

--- OA/operators/mutators.py
+++ OA/operators/mutators.py
@@ -1,58 +1,29 @@
 import random
 
-def single_bit_mutation(individual, p_m):
-
-    mutated_individual = []
-
-    for bit in individual:
-
-        if random.random() < p_m:
-
-            mutated_individual.append(1 - bit)
-
-        else:
-            mutated_individual.append(bit)
-
-
-    return mutated_individual
-
-    # or...
-    # return [1-bit if random.random() < p_m else bit for bit in individual]
-
-
-def buddy_bit_mutation(individual, p_m):
-
-    mutated_individual = []
-
-    i = 0
-
-    while i < len(individual):
-
-        if random.random() < p_m:
-            if not i == len(individual) - 1:
-                mutated_individual.extend([1 - individual[i], 1 - individual[i + 1]])
-            else:
-                mutated_individual.append(1 - individual[i])
-                mutated_individual[0] = 1 - mutated_individual[0]
-
-        else:
-            if not i == len(individual) - 1:
-                mutated_individual.extend([individual[i], individual[i + 1]])
-            else:
-                mutated_individual.append(individual[i])
-
-        i += 1
-
-
-def neighbourhood_mutation(hamming_dist):
-
-    def mutate(individual, p_m):
-        pass
-
-    return mutate
-
-
-
-def test_func():
-    print("hello")
-    pass
+def swap_mutation(tour):
+    """
+    Apply swap mutation to a tour in the TSP population.
+    
+    Parameters:
+    - tour: List representing the tour (list of city indices)
+    
+    Returns:
+    - mutated_tour: List representing the mutated tour
+    """
+    # Make a copy of the tour to avoid modifying the original
+    mutated_tour = tour.deepcopy()
+    
+    # Select two distinct random indices in the tour
+    idx1, idx2 = random.sample(range(len(mutated_tour)), 2)
+    
+    # Swap the cities at the selected indices
+    mutated_tour[idx1], mutated_tour[idx2] = mutated_tour[idx2], mutated_tour[idx1]
+    
+    # Ensure that all cities are visited exactly once
+    mutated_set = set(mutated_tour)
+    for city in tour:
+        if city not in mutated_set:
+            mutated_tour.append(city)
+            break
+    
+    return mutated_tour
