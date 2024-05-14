67d053c931a16559e5596ab21f4fc905f9afc519
M_Marchao <margarida.marchao@gmail.com>
2024-05-14 15:46:36 +0100

    Order

--- OA/operators/crossovers.py
+++ OA/operators/crossovers.py
@@ -51,3 +51,38 @@ def order_xover(p1,p2):
     return c1,c2
 
     
+def pmx_xover(p1,p2):
+    '''
+    Performes partially mapped crossover 
+    '''
+    c1 = p1
+    c2 = p2
+
+    #Choosing size of the middle belt
+    size_middle_belt = random.randint(1, len(p1)//2 -1)
+   
+
+    #Assigning the middle belt to children
+    c1[size_middle_belt: -size_middle_belt] = p2[size_middle_belt: -size_middle_belt]
+    c2[size_middle_belt: -size_middle_belt] = p1[size_middle_belt: -size_middle_belt]
+
+    #Acessing the values present in the belts
+    belt_1 = p1[size_middle_belt: -size_middle_belt]
+    belt_2 = p2[size_middle_belt: -size_middle_belt]
+
+    for i in range(len(c1)):
+        if c1[i] in belt_2:
+            aux = belt_2.index(c1[i])
+            c1[i] = belt_1[aux]
+
+        if c2[i] in belt_1:
+            aux = belt_1.index(c2[i])
+            c2[i] = belt_2[aux]
+
+    return c1,c2
+
+t1 = ["8","4", "7", "3", "6", "2", "5", "1", "9", "0"]
+t2 = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+print(pmx_xover(t1,t2))
