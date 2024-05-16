c05b3d1e0da293843ece2f60b3f7f93a47f0ae45
C-ph3r <margaridaimsardinha@gmail.com>
2024-05-16 11:17:06 +0100

    swapped gen route to gen possible route

--- OA/checking.py
+++ OA/checking.py
@@ -3,15 +3,18 @@ sys.path.insert(0, '..')
 
 from base.population import create_population, evaluate_population
 from operators.selection_algorithms import tournament_selection
+from base.geo_gain_matrix import generate_matrix
+
 areas = ['D', 'FC', 'G', 'QS', 'QG', 'CS', 'KS', 'RG', 'DV', 'SN']
+matrix = generate_matrix(0.8, areas)
 initializer = create_population(areas_list=areas)
-evaluator = evaluate_population()
+evaluator = evaluate_population(matrix)
 
 # 2. Initializing the gen 0 population:
 population = initializer(4)
 
 # 3. Evaluating the current population:
-pop_fit = evaluate_population(population)
+pop_fit = evaluator(population)
 
 print(pop_fit)
 
