815d20de84c1f89b0e3f14952e7b54bccdb87fb7
C-ph3r <margaridaimsardinha@gmail.com>
2024-05-16 10:54:16 +0100

    fixed selections

--- OA/operators/selection_algorithms.py
+++ OA/operators/selection_algorithms.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import math
 
-def tournament_selection(ts=1):
+def tournament_selection(pop, pop_fit, ts=1):
     '''
     Tournament selection algorithm
     
@@ -11,26 +11,22 @@ def tournament_selection(ts=1):
         
     Outputs: list - selected individual
     '''
+    # randomly selecting ts number of individuals from the population:
+    # or, more specifically, choosing the individuals from the population via their index
+    pool = random.choices([i for i in range(len(pop))], k=ts)
 
-    def inner_tournament(pop, pop_fit):
-        # randomly selecting ts number of individuals from the population:
-        # or, more specifically, choosing the individuals from the population via their index
-        pool = random.choices([i for i in range(len(pop))], k=ts)
+    # getting the pop_fit of the individuals of the given index
+    pool_fits = [pop_fit[i] for i in pool]
 
-        # getting the pop_fit of the individuals of the given index
-        pool_fits = [pop_fit[i] for i in pool]
+    # finding out where in the pool fits the best fitness is
+    best = np.argmax(pool_fits)
 
-        # finding out where in the pool fits the best fitness is
-        best = np.argmax(pool_fits)
-
-        # return the individual from the population whose index is the same as the index
-        # in pool of the individual who was best in pool_fits
-        return pop[pool[best]]
-
-    return inner_tournament
+    # return the individual from the population whose index is the same as the index
+    # in pool of the individual who was best in pool_fits
+    return pop[pool[best]]
 
 
-def SUS_selection():
+def SUS_selection(pop, pop_fit, n_sel = 1):
     '''
     Stochastic Universal Sampling (SUS) selection algorithm
     
@@ -39,35 +35,29 @@ def SUS_selection():
         
     Outputs: list - selected individual
     '''
+    fitness_sum = sum(pop_fit)
 
-    def SUS(pop, pop_fit):
-        fitness_sum = sum(pop_fit)
-        num_selections = 1
-
-        # creating selection pointers
-        pointer_distance = fitness_sum / num_selections # Distance between pointers
-        start = random.uniform(0, pointer_distance) # Random start
-        pointers = [start + i * pointer_distance for i in range(num_selections)]    # Evenly spaced
-        
-        selected = []
-        current_fitness = 0
-        index = 0
-        
-        for pointer in sorted(pointers):
-            # Sum fitness values until the pointer threshold
-            while current_fitness < pointer:
-                current_fitness += pop_fit[index]
-                index += 1
-            # Add the corresponding individual to the list 
-            # When only 1 is to be selected, return only the individual
-            selected = pop[index - 1]
-        
-        return selected
+    # creating selection pointers
+    pointer_distance = fitness_sum / n_sel # Distance between pointers
+    start = random.uniform(0, pointer_distance) # Random start
+    pointers = [start + i * pointer_distance for i in range(n_sel)]    # Evenly spaced
     
-    return SUS
+    current_fitness = 0
+    index = 0
+    
+    for pointer in sorted(pointers):
+        # Sum fitness values until the pointer threshold
+        while current_fitness < pointer:
+            current_fitness += pop_fit[index]
+            index += 1
+        # Add the corresponding individual to the list 
+        # When only 1 is to be selected, return only the individual
+        selected = pop[index - 1]
+    
+    return selected
 
 
-def boltzmann_selection(temperature):
+def boltzmann_selection(pop, pop_fit, temperature = 0.5):
     '''
     Boltzmann Selection algorithm for selecting individuals based on entropy
     
@@ -75,21 +65,14 @@ def boltzmann_selection(temperature):
             pop_fit (list) - List of fitness values of the population given
             temperature (float) - Temperature parameter (controls the amount of randomness)
         
-    Output: list - Selected individuals
+    Output: list - Selected individual
     '''
-
-    def boltzmann(pop, pop_fit):
-        num_selections = 1
-        # Calculate Boltzmann probabilities for each individual
-        fitness_sum = sum(math.exp(fit / temperature) for fit in pop_fit)
-        probabilities = [math.exp(individual.fitness / temperature) / fitness_sum for individual in pop]
-        
-        # Perform selection based on Boltzmann probabilities
-        selected = []
-        for _ in range(num_selections):
-            selected = random.choices(pop, weights=probabilities)[0]
-        # in this case only 1 is selected
-        
-        return selected
+    # Calculate Boltzmann probabilities for each individual
+    fitness_sum = sum(math.exp(fit / temperature) for fit in pop_fit)
+    probabilities = [math.exp(individual.fitness / temperature) / fitness_sum for individual in pop]
     
-    return boltzmann
+    # Perform selection based on Boltzmann probabilities
+    selected = random.choices(pop, weights=probabilities)[0]
+    
+    return selected
+    
